39e06a1c2210a6ddf93b7272a5307be27d8c73f5
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-02-09 16:36:23 +0300

    KOKO RIKO!!!
    Trying to figure out to write a do-while or while loop that'll help me start over directly on the console if the user messes up.

--- main.cpp
+++ main.cpp
@@ -1,91 +1,61 @@
 #include <iostream>
-#include <climits>
-#include<cmath>
-#include <float.h>
-#include <string>
-#include <ctime>
-#include <cstdlib>
-#include <vector>
-#include <array>
+#include <string.h>
 
 using namespace std;
 
-void print_array(array<int,251>array, int size)
+
+
+
+
+int main()
 {
-  for (int k=0;k<size;k++)
-  {
-  	cout<<array[k]<<"\t";
-  }
-  cout<<endl;
+  string start;
+  cout<<"Type the word start to get a list of numbers 1 to 100, but with a twist:"<<endl;
+  cout<<"Those divisible by 3 will be replaced by KOKO.\nThose divisible by 5 will be replaced by RIKO.Those divisible by 15 will be replaced by KOKO RIKO."<<endl;
+  getline(cin,start);
+
+
+   if(start=="start"|| start=="Start"||start=="START")
+   {
+      cout<<"Nice! Check below"<<endl;
+      for (int i=1;i<101;i++)
+      {
+         if(i%15==0)
+         {
+           cout<<"KOKO RIKO"<<endl;
+         }
+          else if(i%5==0)
+         {
+           cout<<"RIKO"<<endl;
+         }
+          else if(i%3==0)
+         {
+           cout<<"KOKO"<<endl;
+         }
+           else
+       {
+            cout<<i<<endl;
+       }
+
+      }
+   }
+   else
+   {
+     cout<<"Man,write start!"<<endl;
+
+   }
+
+
+
+
+
+
+
+
 }
 
 
-void play_game()
-{
- array <int,251> guesses;
- int guess_count=0;
- 
- int random=rand() % 251;
- cout<<random<<endl;
- cout<<"Guess a number"<<endl;
- while(true)
- {
-  int guess;
-  cin>>guess;
-  guesses[guess_count++]=guess;
-  
-  if (guess==random)
-    { 
-      cout<<endl<<endl;
-      cout<<guess_count<<" guesses taken."<<endl; 
-      cout<<endl;
-      cout<<"Nice you got the number right!"<<endl<<"Wanna play again or nah?"<<endl;
-      break;
-	}
-  else if (guess<random)
-    {
-  	  cout<<"Sorry your guess was lower than the expected number."<<endl<<"Please try again."<<endl;
-    }
-  else 
-    {
-  	  cout<<"Sorry your guess was higher than the expected number."<<endl<<"Please try again."<<endl;
-    }
- }
- print_array(guesses,guess_count);
-	
-	  	
-	
-}
- 
- 
-  
 
-int main(int argc, char** argv) 
-{   
 
-	
-	
-  srand(time(NULL));
-  int choix;
-  do
-	{
-  cout<<"0.Quit"<<endl<<"1.Play Game"<<endl;
-  cin>>choix;
-  switch(choix)
-		{
- 		 case 0:
-  		cout<<"So sad to see you end the game.\nAnyway, see you next time and thanks for playing."<<endl;
-  		break;
-  		case 1:
-  		play_game();
-  		break;
-  		default:
-  		cout<<"Man, you had one job smh.\nPlease pick 1 or 0 this time, okay?"<<endl;	
-  		break;
-		} 
 
-	}while(choix!=0); 
-	
-	
-	
-}
+
