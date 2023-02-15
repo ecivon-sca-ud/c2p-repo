e8a4a2b8c321a7da2bc81a044736ab830d1bac6f
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-02-15 20:32:18 +0300

    FILE TINGS

--- main.cpp
+++ main.cpp
@@ -1,62 +1,62 @@
 #include <iostream>
-#include <string.h>
+#include <fstream>
+#include<vector>
+#include<string>
 
 using namespace std;
 
 
+ 
+ 
+  
+
+int main(int argc, char** argv) 
+{   
+   string filename;
+   cout<<"Enter name of file:"<<"\t";
+   getline(cin,filename);
 
 
-
-int main()
-{ 
-  int start;
-  do
-  {
-  cout<<"Type 0 to get a list of numbers 1 to 100, but with a twist:"<<endl;
-  cout<<"Those divisible by 3 will be replaced by KOKO.\nThose divisible by 5 will be replaced by RIKO.Those divisible by 15 will be replaced by KOKO RIKO."<<endl;
-  cin>>start;
-
-
-   if(start==0)
+   ofstream file;
+   file.open(filename.c_str(),ios::app);
+   
+   if (file.is_open())
    {
-      cout<<"Nice! Check below"<<endl;
-      for (int i=1;i<101;i++)
-      {
-         if(i%15==0)
-         {
-           cout<<"KOKO RIKO"<<endl;
-         }
-          else if(i%5==0)
-         {
-           cout<<"RIKO"<<endl;
-         }
-          else if(i%3==0)
-         {
-           cout<<"KOKO"<<endl;
-         }
-        
-           else
-       {
-            cout<<i<<endl;
-       } 
-
-      }
-      break;
+   
+   cout<<"Check file named  "<<filename<<" ."<<endl;
+   file<<"C++ is tricky"<<endl;
+   vector<string>names;
+   names.push_back("Jimmy");
+   names.push_back("Timmy");
+   names.push_back("Aaron");
+   names.push_back("Norah");
+   names.push_back("Mosoti");
+  
+  for(string name:names)
+   {
+  	 file<<name<<endl;
    }
-   else
-   {
-    cout<<"Man,type zero!\nTry again..."<<endl;
-   		
-	}
-     
-
-   } while(start=!0);
-
-
-
-
-
-
-
-
+  }
+  else
+  {
+  cout<<"Apologies,unable to open file"<<endl;	
+  }
+  
+  
+  
+  ifstream file ("Warrup.txt");
+  vector<string>names;
+  string input;
+    
+  while(file>>input)  //returns the file,hopefully!
+  {
+  	names.push_back(input);
+  }
+  
+  for(string name:names)
+  {
+  	cout<<name<<endl;
+  }
+  
+  
 }
