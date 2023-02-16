a633c27c600f46a9f969e1e85634406938f1bf5b
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-02-16 16:24:15 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,62 +1,63 @@
 #include <iostream>
-#include <fstream>
-#include<vector>
-#include<string>
+#include <string.h>
+#include <vector>
+#include <math.h>
+
 
 using namespace std;
 
 
- 
- 
-  
-
-int main(int argc, char** argv) 
-{   
-   string filename;
-   cout<<"Enter name of file:"<<"\t";
-   getline(cin,filename);
 
 
-   ofstream file;
-   file.open(filename.c_str(),ios::app);
-   
-   if (file.is_open())
-   {
-   
-   cout<<"Check file named  "<<filename<<" ."<<endl;
-   file<<"C++ is tricky"<<endl;
-   vector<string>names;
-   names.push_back("Jimmy");
-   names.push_back("Timmy");
-   names.push_back("Aaron");
-   names.push_back("Norah");
-   names.push_back("Mosoti");
-  
-  for(string name:names)
-   {
-  	 file<<name<<endl;
-   }
-  }
-  else
-  {
-  cout<<"Apologies,unable to open file"<<endl;	
-  }
-  
-  
-  
-  ifstream file ("Warrup.txt");
-  vector<string>names;
-  string input;
-    
-  while(file>>input)  //returns the file,hopefully!
-  {
-  	names.push_back(input);
-  }
-  
-  for(string name:names)
-  {
-  	cout<<name<<endl;
-  }
-  
-  
+
+int main()
+{
+    /*
+    vector<int>position={6,7,8,9,10};
+    for(int ind: position)
+    {
+        cout<<ind<<endl;
+    }
+    */
+
+
+    /*
+    bool ismale=true;
+    if(ismale)
+    {
+        cout<<boolalpha<<ismale<<endl;
+    }
+    else
+    {
+        cout<<boolalpha<<!ismale<<endl;
+    }
+    */
+
+
+      /*
+      enum month{Jan,Feb=4,Mar,Apr,May=8,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
+     cout<<(month) <<endl;
+     */
+
+
+     int a=5;
+     int *p;
+     p=&a;
+     cout<<a<<endl<<p<<endl;
+    cout<<*p * *p<<endl;
+    *p=21;
+    cout<<*p<<endl;
+    cout<<pow(*p,3)<<endl;
+    int y=10;
+    int *p2=&y;
+    cout<<pow(*p2,2)<<endl<<pow(*p2+1,3)<<endl;
+
+
+
 }
+
+
+
+
+
+
