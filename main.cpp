853291bb526b447b53b03cc275cbfa57e0f413b5
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-02-24 15:49:20 +0300

    Guessing Game, now with the mess of files

--- main.cpp
+++ main.cpp
@@ -1,63 +1,118 @@
 #include <iostream>
-#include <string.h>
+#include <climits>
+#include<cmath>
+#include <float.h>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 #include <vector>
-#include <math.h>
+#include <fstream>
 
 
 using namespace std;
 
-
-
-
-
-int main()
+void print_vector(vector<int>vector)
 {
-    /*
-    vector<int>position={6,7,8,9,10};
-    for(int ind: position)
-    {
-        cout<<ind<<endl;
-    }
-    */
-
-
-    /*
-    bool ismale=true;
-    if(ismale)
-    {
-        cout<<boolalpha<<ismale<<endl;
-    }
-    else
-    {
-        cout<<boolalpha<<!ismale<<endl;
-    }
-    */
-
-
-      /*
-      enum month{Jan,Feb=4,Mar,Apr,May=8,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
-     cout<<(month) <<endl;
-     */
-
-
-     int a=5;
-     int *p;
-     p=&a;
-     cout<<a<<endl<<p<<endl;
-    cout<<*p * *p<<endl;
-    *p=21;
-    cout<<*p<<endl;
-    cout<<pow(*p,3)<<endl;
-    int y=10;
-    int *p2=&y;
-    cout<<pow(*p2,2)<<endl<<pow(*p2+1,3)<<endl;
-
-
-
+  for (int k=0;k<vector.size();k++)
+  {
+  	cout<<vector[k]<<"\t";
+  }
+  cout<<endl;
 }
 
 
+void play_game()
+{
+ vector <int> guesses;
+ int count=0;
+ 
+ 
+ int random=rand() % 251;
+ cout<<random<<endl;
+ cout<<"Guess a number"<<endl;
+ while(true)
+ {
+  int guess;
+  cin>>guess;
+  count++;     //Used to keep track of number of guesses from each round. Best used with a file that stores this, as will soon be seen.
+  
+  guesses.push_back(guess);
+  
+  if (guess==random)
+    { 
+      cout<<endl<<endl;
+      cout<<"Nice you got the number right!"<<endl<<"Wanna play again or nah?"<<endl;
+      break;
+	}
+  else if (guess<random)
+    {
+  	  cout<<"Sorry your guess was lower than the expected number."<<endl<<"Please try again."<<endl;
+    }
+  else 
+    {
+  	  cout<<"Sorry your guess was higher than the expected number."<<endl<<"Please try again."<<endl;
+    }
+ }
+   std::fstream input("Best Scores.txt");
+   
+   int best_score;
+   input>>best_score;
+   
+   std::fstream output("Best Scores.txt");
+   if(!output.is_open())
+   {
+   	cout<<"Unable to read from file"<<endl;
+   }
+   
+   if(count<best_score)
+   {
+   	output<<endl;
+   	output<<count<<endl;
+   }
+   else
+   {
+   	output<<endl;
+   	output<<best_score<<endl;
+   }
+ 
+ 
+ 
 
+	
+	  	
+  print_vector(guesses);	
+}
+ 
+ 
+  
 
+int main(int argc, char** argv) 
+{   
 
+	
+	
+  srand(time(NULL));
+  int choix;
+  do
+	{
+  cout<<"0.Quit"<<endl<<"1.Play Game"<<endl;
+  cin>>choix;
+  switch(choix)
+		{
+ 		 case 0:
+  		cout<<"So sad to see you end the game.\nAnyway, see you next time and thanks for playing."<<endl;
+  		break;
+  		case 1:
+  		play_game();
+  		break;
+  		default:
+  		cout<<"Man, you had one job smh.\nPlease pick 1 or 0 this time, okay?"<<endl;	
+  		break;
+		} 
+
+	}while(choix!=0); 
+	
+	
+	
+}
 
