1e315d12512581791e9e74be5c70ebdaf93bc349
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-03-23 16:52:21 +0300

    Shape tings (Protected Inheritance Experiment)

--- main.cpp
+++ main.cpp
@@ -1,91 +1,61 @@
 #include <iostream>
-#include<math.h>
 #include <string>
-#include <vector>
 
+#define pi 3.141592654
 
 using namespace std;
 
-class Student
-{
-private:
-    string name;
-    int age;
+class Shape
+ {
+protected:
+     double area;
+ } ;
+
+ class  Circle : protected Shape
+ {
+ private:
+    double radius,circumference;
 public:
-    Student()
-    {
-
-    }
-   void setData(string n,int a)
-    {
-        name=n;
-        age=a;
-    }
-    void Display()
-    {
-        cout<<"Name: " <<name<<endl;
-        cout<<"Age:    "<<age<<endl;
-    }
-    /*void DisplayName()
-    {
-        cout<<"Name: " <<name<<endl;
-    }*/
-
-    int getAge()
-    {
-        return age;
-    }
+     void getCCalc()
+     {
+            cout<<"Enter radius in cm."<<endl;
+            cin>>radius;
+            area=pi*radius*radius;
+            circumference=2*pi*radius;
+     }
+     void setCCalc()
+     {
+            cout<<"Circle's circumference= "<<circumference<<"  cm."<<endl;
+            cout<<"Circle's area= "<<area<<" squared cm."<<endl;
+     }
 
 
+ };
 
-
-};
+ class Square : protected Shape
+ {
+private:
+     double s_side;
+public:
+     void getSArea()
+     {
+         cout<<"Enter square's length in cm"<<endl;
+         cin>>s_side;
+         area=s_side*s_side;
+     }
+     void setSArea()
+     {
+         cout<<"Square's area="<<area<<" squared cm."<<endl;
+     }
+ };
 
 
 int main()
 {
-    int num;
-    int sum=0;
-    string name;
-    int age;
-    cout<<"How many students are we working with?"<<endl;
-    cin>>num;
-    Student student[num];
-
-    for (int i=1;i<=num;i++)
-    {
-        cout<<"Name?"<<endl;
-        cin>>name;
-        cout<<"Age?"<<endl;
-        cin>>age;
-        sum=sum+age;
-        student[i].setData(name,age);
-    }
-
-    for (int j=1;j<=num;j++)
-    {
-        student[j].Display();
-    }
-    //cout<<sum<<endl;
-
-   for (int k=1;k<=num;k++)
-   {
-       sum=sum+age;
-   }
-    cout<<"Average age= "<<(double)sum/num<<endl;
-
-
-
-
-
-
-
-
+        Circle circ1;
+        circ1.getCCalc();
+        circ1.setCCalc();
+        Square sq1;
+        sq1.getSArea();
+        sq1.setSArea();
 }
-
-
-
-
-
-
-
